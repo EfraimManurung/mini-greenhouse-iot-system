ed1d771f747e3cecf9373799401ff7fc0b171cd6
EfraimManurung <frm199911@gmail.com>
2024-09-22 22:59:26 +0200

    add more information for debugging

--- debug_hw.py
+++ debug_hw.py
@@ -2,8 +2,15 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+# Ventilation
 GPIO.setup(26, GPIO.OUT)
+
+# Toplights
 GPIO.setup(17, GPIO.OUT)
+
+# Heater
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
 
 fan  = GPIO.PWM(26, 50)
 ledStrip = GPIO.PWM(17, 50)
